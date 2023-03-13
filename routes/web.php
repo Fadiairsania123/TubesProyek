b79f2a1e4164efc43590cfd6f7024bd9634bae44
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2023-03-14 01:52:56 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AboutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,15 +17,14 @@ use App\Http\Controllers\LoginController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
 
 Route::get('/login', [LoginController::class,'index'])->name('login');
 Route::post('/login', [LoginController::class,'login'])->name('login.action');
 Route::get('/logout',[LoginController::class,'logout'])->name('logout');
 Route::post('/register', [LoginController::class,'register'])->name('register.action');
-Route::get("/home",[HomeController::class,"index"])->name('home');
-Route::get("/about",[AboutController::class,"index"])->name('about');
+
 
 // verifikasi registrasi
 Route::get('/activation', [LoginController::class,'activation'])->name('activation');
@@ -36,3 +36,10 @@ Route::post('/forgot',[LoginController::class,'reset_action'])->name('forgot.act
 Route::get('/forgot-confirm',[LoginController::class,'confirm_email'])->name('confirm_email');
 Route::get("verify-email-reset/{user_token}", [LoginController::class,'verifyReset']) -> name('verifyLinkReset');
 Route::post("verify-email-reset", [LoginController::class,'reset_password']) -> name('verifyLinkResetAction');
+
+Route::group(['middleware' => ["Login"]], function () {
+
+    Route::get("/home",[HomeController::class,"index"])->name('home');
+    Route::get("/about",[AboutController::class,"index"])->name('about');
+
+});
